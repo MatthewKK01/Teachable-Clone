af918fc7c4da207af29f6abc5c2280f0d43f679a
Mate Kavelashvili <matthewkk@Mates-MacBook-Air.local>
2023-06-20 17:53:58 +0400

    fixed headers burger menu

--- src/app/common/Header.tsx
+++ src/app/common/Header.tsx
@@ -1,13 +1,15 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+
+import React, { useState } from "react";
 import Image from "next/image";
 
-import { RxHamburgerMenu } from "react-icons/rx";
-
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="flex items-center justify-between py-7 px-6 border-b border-black">
-      <div>
+      <div className="flex items-center gap-8">
         <Link href="/">
           <Image
             alt="teachable logo"
@@ -16,10 +18,39 @@ export default function Header() {
             src="https://www.datocms-assets.com/67254/1659386300-teachable_logo-header.svg"
           />
         </Link>
+        <nav className="hidden lg:block">
+          <ul className="flex gap-8 font-basisRegular text-base">
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Blog</li>
+            <li>Examples</li>
+            <li>Resources</li>
+          </ul>
+        </nav>
       </div>
 
-      <div>
-        <button>
+      {isOpen && (
+        <nav className="absolute lg:hidden bg-white top-0 bottom-0 left-0 right-0 z-50 mt-20 p-8">
+          <ul className="text-2xl flex flex-col gap-8">
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Blog</li>
+            <li>Examples</li>
+            <li>Resources</li>
+          </ul>
+        </nav>
+      )}
+
+      <div className="hidden lg:flex lg:gap-8">
+        <button>Login</button>
+        <button>Join for free</button>
+      </div>
+
+      <div className="lg:hidden">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="menu-button flex gap-[6px]   flex-col"
+        >
           <span></span>
           <span></span>
           <span></span>
